60953e22ba23ccc81c86d9f5d65291ca57bc7de1
seb.and156 <seb.and156@gmail.com>
2025-10-29 21:02:51 +0000

    📦	build: Se instala Nativewind -Se instaña Nativewind para no utilizar la etiqueta <style>

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import "@/global.css";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-blue-500">
+    <View>
+      <Text className="text-2xl font-bold text-center">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+    </View>
+  );
+}
